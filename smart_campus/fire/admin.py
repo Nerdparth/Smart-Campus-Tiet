517b668a92e52510b39a8d0750cad5f25dc68f4a
ashboi005 <125044617+ashboi005@users.noreply.github.com>
2024-11-14 22:52:13 +0530

    changed index to show fire api endpoints

--- smart_campus/fire/admin.py
+++ smart_campus/fire/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import FireSensor
 
-# Register your models here.
+@admin.register(FireSensor)
+class FireSensorAdmin(admin.ModelAdmin):
+    list_display = ('sensor_id', 'location')
