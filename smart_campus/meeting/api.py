8129006b9358243f4dcbd0d3a32bf6e1646f6f29
ashboi005 <125044617+ashboi005@users.noreply.github.com>
2024-11-16 00:44:47 +0530

    added get meeting api

--- smart_campus/meeting/api.py
+++ smart_campus/meeting/api.py
@@ -1,6 +1,7 @@
 from ninja import NinjaAPI
 from .models import Meeting
 from .schemas import MeetingURLSchema
+from django.shortcuts import get_object_or_404
 
 meeting = NinjaAPI(urls_namespace="meeting_api")
 
@@ -17,3 +18,10 @@ def set_meeting_url(request, payload: MeetingURLSchema):
         return "Meeting URL saved successfully."
     else:
         return "Meeting URL updated successfully."
+
+
+@meeting.get("/get_url", response=MeetingURLSchema)
+def get_meeting_url(request):
+    """Retrieve the latest meeting URL."""
+    meeting_instance = get_object_or_404(Meeting, id=1)
+    return {"url": meeting_instance.url}
